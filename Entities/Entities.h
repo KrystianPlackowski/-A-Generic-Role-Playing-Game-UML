16cbca1e27d69f36e567f9aa75be3680f2f1e098
Lukasz Laniewski-Wollk <llaniewski@meil.pw.edu.pl>
2018-11-05 20:26:57 +0100

    Adding first source

---
+++ Entities/Entities.h
@@ -0,0 +1,5 @@
+
+// Include here all the needed headers of your entities library
+// #include "Entity.h"
+// #include "Character.h"
+// #include "..."
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include "Entities/Entities.h"
+#include <iostream>
+
+int main() {
+  std::cout << "Let's start ...\n";
+}
