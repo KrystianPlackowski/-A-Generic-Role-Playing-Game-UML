528d720177e66a96ba7511a5346be9c01dcdfaab
Lukasz Laniewski-Wollk <llaniewski@meil.pw.edu.pl>
2018-11-05 20:35:53 +0100

    Added example

--- README.md
+++ README.md
@@ -11,3 +11,7 @@ Dodaj wszystkie pliki w katalogu `Entities`, dzieląc klasy na pliki `.h` i `.cp
 Załącz wszystkie potrzebne pliki nagłówkowe w `Entities/Entities.h`.
 
 ![UML diagram](uml.png)
+
+## Requirements
+
+Only files in the `Entities` directory will be considered as solution.
--- main.cpp
+++ main.cpp
@@ -2,5 +2,24 @@
 #include <iostream>
 
 int main() {
-  std::cout << "Let's start ...\n";
+  Entity * elf = new Elf();
+  std::cout << elf.getDescription() << "\n";
+  delete elf;
+
+  Entity * halfwitOrc = new HalfWit(new Orc());
+  std::cout << halfwitOrc.getDescription() << "\n";
+  delete halfwitOrc;
+  
+  Entity * armoredOrc = new Orc();
+  armoredOrc.wear(new PlateArmor());
+  armoredOrc.wield(new Brick());
+  armoderOrc.carry(new Axe());
+  armoderOrc.carry(new TireBouchon());
+  std::cout << armoredOrc.getDescription() << "\n";
+  delete armoredOrc;
+
+  Entity * heavyElf = new Elf();
+  for (int i=0; i<30; i++) heavyElf.carry(new Brick());
+  std::cout << heavyElf.getDescription() << "\n";
+  delete heavyElf;
 }
