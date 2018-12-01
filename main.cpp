a57deea05c8ea0935741ebdcf43c613abf832efe
KrystianPlackowski <kpchris20@gmail.com>
2018-12-01 19:43:21 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,27 +1,36 @@
 #include "Entities/Entities.h"
+#include <stdio.h>
 #include <iostream>
 
 int main() {
-  Character * orc = new Orc();
-  std::cout << "I'm " << orc->getDescription() << ".\n";
-  orc->wield(new Brick());
-  std::cout << "I'm " << orc->getDescription() << ".\n";
-  orc->carry(new Socks());
-  std::cout << "I'm " << orc->getDescription() << ".\n";
-  orc = new HalfWit(orc);
-  std::cout << "I'm " << orc->getDescription() << ".\n";
-  orc->setName("John");
-  std::cout << "I'm " << orc->getDescription() << ".\n";
-  orc->wear(new PlateArmor());
-  std::cout << "I'm " << orc->getDescription() << ".\n";
-  orc->carry(new SeveredHand());
-  std::cout << "I'm " << orc->getDescription() << ".\n";
+	
+	
 
-  std::cout << "My weight is "      << orc->getWeight() << ".\n";
-  std::cout << "My strength is "    << orc->getStrength() << ".\n";
-  std::cout << "My inteligence is " << orc->getInteligence() << ".\n";
-  std::cout << "My dexterity is "   << orc->getDexterity() << ".\n";
-  std::cout << "My speed is "       << orc->getSpeed() << ".\n";
-  std::cout << "My armor is "       << orc->getArmor() << ".\n";
-  std::cout << "My power is "       << orc->getPower() << ".\n";
+	Character * orc = new Orc();
+	
+	cout << orc->getDescription() << endl;
+	
+	orc->wield(new Brick());
+	
+	cout << orc->getDescription() << endl;
+	
+	orc->carry(new Socks());
+	
+	cout << orc->getDescription() << endl;
+	
+	orc = new HalfWit(orc);
+	
+	cout << orc->getDescription() << endl;
+	
+	orc->setName("John");
+	
+	cout << orc->getDescription() << endl;
+	
+	orc->wear(new PlateArmor());
+	
+	cout << orc->getDescription() << endl;
+
+	cout << orc->getJSON() << endl;
+
+	return 0;
 }
